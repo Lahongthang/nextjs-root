be7c7d92d63df437234ea83a88b2174309fc5523
Thangla <lahongthang266@gmail.com>
2024-06-14 15:55:39 +0700

    Restructure stories folder, customize story button

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,21 @@
 'use client';
 
+import { useTranslation } from 'react-i18next';
+import { useRouter } from 'next/navigation';
+import { Button } from '@/stories/button/Button';
 import LanguageSwitcher from './LanguageSwitcher';
-import SignInBtn from './SignInBtn';
 
 export default function Header() {
+  const { t } = useTranslation();
+  const router = useRouter();
   return (
     <div className="header">
-      <SignInBtn />
+      <Button
+        primary
+        label={t('header.signInBtn')}
+        size="small"
+        onClick={() => router.push('/auth/sign-in')}
+      />
       <LanguageSwitcher />
     </div>
   );
